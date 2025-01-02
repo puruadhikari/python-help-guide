6b9f92f03201a609eb5dc28766fe36c2e2646002
puruadhikari <puru1977@gmail.com>
2025-01-02 16:16:28 +0000

    additional leetcode problem

---
+++ help-docs/heapq_help.py
@@ -0,0 +1,14 @@
+import heapq
+
+nums = [6, 7, 9, 4, 3, 5, 8, 10, 1]
+
+#heapq.heapify(nums)
+
+print(nums) # prints - [1, 3, 5, 4, 6, 9, 8, 10, 7]
+
+# find kth largest elements
+val = heapq.nlargest(2,nums)
+print(val) # prints - [10,9]
+
+val_s = heapq.nsmallest(2, nums)
+print(val_s) # prints - [1,3]
